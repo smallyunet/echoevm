5c46dfa84b3758c5452d300cef6483a1ae5021ed
smallyunet <dev.wangyu@gmail.com>
2025-06-08 22:56:27 +0800

    modify add.sol contract
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- test/contracts/Add.sol
+++ test/contracts/Add.sol
@@ -2,10 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract Add {
-    function add() public pure returns (uint) {
-        return 5 + 6;
-    }
-
     // add allows addition of two parameters to demonstrate
     // calldata usage when running runtime bytecode.
     function add(uint256 a, uint256 b) public pure returns (uint256) {
