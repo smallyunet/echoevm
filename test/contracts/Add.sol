4a6b8812d0909a93648c31d3c5fc5776e54b7bea
smallyunet <dev.wangyu@gmail.com>
2025-07-05 17:48:13 +0800

    add contract test folder
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- test/contracts/Add.sol
+++ test/contracts/Add.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract Add {
-    // add allows addition of two parameters to demonstrate
-    // calldata usage when running runtime bytecode.
     function add(uint256 a, uint256 b) public pure returns (uint256) {
         return a + b;
     }
