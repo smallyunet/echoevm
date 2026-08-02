0b0ece130104dbb26a253e69d8aa2c4966bfa9a5
smallyunet <dev.wangyu@gmail.com>
2026-08-02 19:35:54 +0800

    test: update release version expectation

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -72,7 +72,7 @@ func TestE2E_Run(t *testing.T) {
 		{
 			name:     "version",
 			args:     []string{"version"},
-			wantOut:  "echoevm v0.0.31",
+			wantOut:  "echoevm v0.0.34",
 			wantCode: 0,
 		},
 		{
